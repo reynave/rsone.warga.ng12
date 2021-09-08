8ae00896964decef53e17bbf281080c993d56c83
reynave <felix.renaldi@gmail.com>
2021-09-08 23:00:57 +0700

    Update By Felix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  api : "https://forwards.or.id/warga.api.uat/",  
-  apiAdmin : "https://forwards.or.id/admin.api.uat/formresidenceone/", 
+ // api : "https://forwards.or.id/warga.api.uat/",  
+ // apiAdmin : "https://forwards.or.id/admin.api.uat/formresidenceone/", 
+  api : "http://localhost/application/resident.one/api-warga/",
+  apiAdmin : "http://localhost/application/resident.one/api-user/formresidenceone/", 
 };
 
 /*
