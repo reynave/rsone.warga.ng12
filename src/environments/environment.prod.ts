e5d5dd997286a5f73b2639d1db1477a0234ba982
reynave <felix.renaldi@gmail.com>
2021-09-09 10:59:52 +0700

    update ENV by Felix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  api : "https://forwards.or.id/warga.api.uat/",
-  //api : "https://forwards.or.id/warga.api.uat/", 
-  apiAdmin : "https://forwards.or.id/admin.api.uat/formresidenceone/",
-  //apiAdmin : "https://forwards.or.id/admin.api.uat/formresidenceone/", 
+  api : "https://forwards.or.id/warga.api/",
+  apiAdmin : "https://forwards.or.id/admin.api/formresidenceone/", 
 };
